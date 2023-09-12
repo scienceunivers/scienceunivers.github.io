30c845eb5422ecd07ad9a1725f37641f51a7dff7
scienceunivers <57838591+scienceunivers@users.noreply.github.com>
2023-09-12 15:22:15 +0800

    uploads songs, pictures and change music-list and modify file path in index.html
    uploads songs, pictures, change music-list, modify js, css file path in 听首歌.html

--- files/Music Player in JavaScript/js/music-list.js
+++ files/Music Player in JavaScript/js/music-list.js
@@ -11,40 +11,187 @@
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1"
+    name: 'Ai-Sareru-Hana-Ai-Sarenu-Hana',
+    artist: '',
+    img: 'music-1',
+    src: 'Ai-Sareru-Hana-Ai-Sarenu-Hana'
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
-    img: "music-2",
-    src: "music-2"
+    name: 'AnnieS-Song-Placido-Domingo',
+    artist: '',
+    img: 'music-1',
+    src: 'AnnieS-Song-Placido-Domingo'
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
-    img: "music-3",
-    src: "music-3"
+    name: 'Beyond-情人-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Beyond-情人-192-kbps'
   },
   {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
-    img: "music-4",
-    src: "music-4"
+    name: 'Capozio-Inspire-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Capozio-Inspire-192-kbps'
   },
   {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
-    img: "music-5",
-    src: "music-5"
+    name: 'DAISHI-DANCE-Cecile-Corbel-Take-Me-Hand',
+    artist: '',
+    img: 'music-1',
+    src: 'DAISHI-DANCE-Cecile-Corbel-Take-Me-Hand'
   },
   {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
-    img: "music-6",
-    src: "music-6"
+    name: 'Enya-Only-Time-Lyrics',
+    artist: '',
+    img: 'music-1',
+    src: 'Enya-Only-Time-Lyrics'
+  },
+  {
+    name: 'Faith-Hill-There-youll-be-lyrics-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Faith-Hill-There-youll-be-lyrics-192-kbps'
+  },
+  {
+    name: 'GERARD-DARMON-_-AND-THE-WINNER-IS-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'GERARD-DARMON-_-AND-THE-WINNER-IS-192-kbps'
+  },
+  {
+    name: 'Nothings-Gonna-Change-My-Love-for-You-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Nothings-Gonna-Change-My-Love-for-You-192-kbps'
+  },
+  {
+    name: 'Right-Here-Waiting-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Right-Here-Waiting-192-kbps'
+  },
+  {
+    name: 'Skylar-Grey-Coming-Home-Pt-II-Official-Video-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Skylar-Grey-Coming-Home-Pt-II-Official-Video-192-kbps'
+  },
+  {
+    name: 'Trademark-only-love-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: 'Trademark-only-love-192-kbps'
+  },
+  { name: '下一个天亮郭静', artist: '', img: 'music-1', src: '下一个天亮郭静' },
+  {
+    name: '不浪漫罪名-王杰-歌词版-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '不浪漫罪名-王杰-歌词版-192-kbps'
+  },
+  {
+    name: '周杰倫-Jay-Chou【給我一首歌的時間-Give-me-the-time-of-a-song】-Official-Music-Video-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '周杰倫-Jay-Chou【給我一首歌的時間-Give-me-the-time-of-a-song】-Official-Music-Video-192-kbps'
+  },
+  {
+    name: '天边-戴玉强192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '天边-戴玉强192-kbps'
+  },
+  { name: '廬州月', artist: '', img: 'music-1', src: '廬州月' },
+  {
+    name: '开不了口-周杰伦-歌词版-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '开不了口-周杰伦-歌词版-192-kbps'
+  },
+  {
+    name: '張雨生《口是心非》官方MV-Official-Music-Video-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '張雨生《口是心非》官方MV-Official-Music-Video-192-kbps'
+  },
+  {
+    name: '恰似你的温柔-鄧麗君-邓丽君-Teresa-Teng',
+    artist: '',
+    img: 'music-1',
+    src: '恰似你的温柔-鄧麗君-邓丽君-Teresa-Teng'
+  },
+  {
+    name: '时光-许巍-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '时光-许巍-192-kbps'
+  },
+  {
+    name: '星晴-周杰伦-歌词版-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '星晴-周杰伦-歌词版-192-kbps'
+  },
+  {
+    name: '枫-周杰伦-歌词版-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '枫-周杰伦-歌词版-192-kbps'
+  },
+  {
+    name: '游山恋-_-遊山戀-海伦【我欲迎风在留住几步-怎舍寒风吹动我痛处】古风-中国风-抖音-中文歌曲-华语歌曲-_-Chinese-Song-Chinese-Music-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '游山恋-_-遊山戀-海伦【我欲迎风在留住几步-怎舍寒风吹动我痛处】古风-中国风-抖音-中文歌曲-华语歌曲-_-Chinese-Song-Chinese-Music-192-kbps'
+  },
+  {
+    name: '王力宏-依然爱你-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '王力宏-依然爱你-192-kbps'
+  },
+  {
+    name: '花海-中孝介-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '花海-中孝介-192-kbps'
+  },
+  {
+    name: '蔡依林-布拉格广场『a-a-fu-shou』【動態歌詞Lyrics】-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '蔡依林-布拉格广场『a-a-fu-shou』【動態歌詞Lyrics】-192-kbps'
+  },
+  {
+    name: '蔡琴-讀你-Read-You-By-Tsai-Chin',
+    artist: '',
+    img: 'music-1',
+    src: '蔡琴-讀你-Read-You-By-Tsai-Chin'
+  },
+  {
+    name: '街道辦、歐陽耀瑩-春嬌與志明【抖音裡的粵語RAP】【動態歌詞Lyrics】-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '街道辦、歐陽耀瑩-春嬌與志明【抖音裡的粵語RAP】【動態歌詞Lyrics】-192-kbps'
+  },
+  {
+    name: '郭靜-Claire-Kuo-每一天都不同-官方版MV-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '郭靜-Claire-Kuo-每一天都不同-官方版MV-192-kbps'
+  },
+  {
+    name: '高梨康治-五月雨-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '高梨康治-五月雨-192-kbps'
+  },
+  { name: '그속에서-在其中', artist: '', img: 'music-1', src: '그속에서-在其中' },
+  {
+    name: '들국화-걱정말아요-그대-192-kbps',
+    artist: '',
+    img: 'music-1',
+    src: '들국화-걱정말아요-그대-192-kbps'
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
